d67d11f40758aa4fc60e771f14c98dbb673b7308
lucascarrr <lukegabrielcarr@gmail.com>
2025-10-29 10:12:15 +0100

    test

--- lua/lush_theme/vim-earl-grey.lua
+++ lua/lush_theme/vim-earl-grey.lua
@@ -10,29 +10,29 @@
 --  ,aa,_88        I8      8I   ,8'8,    I8dP" "8I
 -- dP" "88P        I8,    ,8I  ,8'  Yb   I8P    I8
 -- Yb,_,d88b,,_   ,d8b,  ,d8b,,8'_   8) ,d8     I8,
---  "Y8P"  "Y888888P'"Y88P"`Y8P' "YY8P8P88P     `Y8
+--  "Y8P"  "Y888888P'"Y88P`Y8P' "YY8P8P88P     `Y8
 --
-
 local lush = require "lush"
 local hsl = lush.hsl
-
 local palette = {
   foreground = hsl "#45363b",
   background = hsl "#d4ccb9",
-  background_alt = hsl "#F7F3EE",
-  purple = hsl "#83577D",
-  blue = hsl "#556995",
-  teal = hsl "#477A7B",
-  green = hsl "#747B4D",
-  red = hsl "#8F5652",
-  orange = hsl "#886A44",
+  background_alt = hsl "#c4bcaa",
+  purple = hsl "#96522b",
+  blue = hsl "#416978",
+  teal = hsl "#416978",
+  green = hsl "#858062",
+  red = hsl "#bd100d",
+  orange = hsl "#e9a448",
+  selection = hsl "#958b83",
+  gray = hsl "#5e5252",
+  light_gray = hsl "#98999c",
 }
-
 ---@diagnostic disable: undefined-global
 local theme = lush(function()
   return {
     Normal { bg = palette.background, fg = palette.foreground },
-    Comment { fg = palette.foreground.lighten(35) },
+    Comment { fg = palette.gray },
     EndOfBuffer { fg = Comment.fg },
     LineNr { fg = Comment.fg },
     Keyword { fg = palette.purple },
@@ -52,58 +52,48 @@ local theme = lush(function()
     Function { fg = palette.foreground },
     PreProc { fg = palette.orange },
     Statement { fg = palette.purple },
-    NormalFloat { bg = palette.background_alt.darken(3), fg = palette.foreground },
+    NormalFloat { bg = palette.background_alt, fg = palette.foreground },
     DiffDelete { fg = palette.red },
     DiffAdd { fg = palette.green },
     DiffChange { fg = palette.orange },
     VertSplit { fg = palette.foreground },
-    Visual { bg = palette.blue.lighten(70) },
+    Visual { bg = palette.selection },
     Search { bg = palette.purple, fg = palette.background },
     IncSearch { bg = palette.blue, fg = palette.background },
-    NonText { fg = palette.blue.lighten(15) },
+    NonText { fg = palette.light_gray },
     SpecialKey { fg = NonText.fg },
     Directory { fg = palette.teal },
-
     Title { fg = palette.teal, gui = "bold" },
     htmlH2 { fg = palette.blue, gui = "bold" },
     NormalNB { bg = palette.background_alt },
-
     Cursor { bg = palette.foreground, fg = palette.background },
     MatchParen { bg = Visual.bg, gui = "underline" },
-
     CursorLine { bg = palette.background_alt },
     ColorColumn { bg = palette.background_alt },
     SignColumn { bg = palette.bg },
     CursorLineNr { bg = palette.background_alt },
-
     Bold { gui = "bold" },
     Underlined { gui = "underline" },
     Italic { gui = "italic" },
-
     Pmenu { bg = palette.background_alt },
-    PmenuSel { bg = palette.background_alt.darken(15) },
-
-    Todo { bg = palette.background_alt.darken(3), fg = palette.teal },
-
+    PmenuSel { bg = palette.selection },
+    Todo { bg = palette.background_alt, fg = palette.teal },
     SpellBad { gui = "underline", fg = palette.red },
     SpellCap { gui = "underline", fg = palette.blue },
     SpellRare { gui = "underline", fg = palette.orange },
     SpellLocal { gui = "underline", fg = palette.purple },
-
     DiagnosticError { fg = palette.red },
     DiagnosticWarn { fg = palette.orange },
     DiagnosticHint { fg = palette.blue },
     DiagnosticInfo { fg = palette.purple },
-    DiagnosticUnderlineError { gui = "underline", sp = palette.red.lighten(10) },
-    DiagnosticUnderlineWarn { gui = "underline", sp = palette.orange.lighten(10) },
-    DiagnosticUnderlineHint { gui = "underline", sp = palette.blue.lighten(10) },
-    DiagnosticUnderlineInfo { gui = "underline", sp = palette.purple.lighten(10) },
-
+    DiagnosticUnderlineError { gui = "underline", sp = palette.red },
+    DiagnosticUnderlineWarn { gui = "underline", sp = palette.orange },
+    DiagnosticUnderlineHint { gui = "underline", sp = palette.blue },
+    DiagnosticUnderlineInfo { gui = "underline", sp = palette.purple },
     LspReferenceText { bg = palette.background_alt },
     LspReferenceRead { bg = palette.background_alt },
     LspReferenceWrite { bg = palette.background_alt },
   }
 end)
-
 -- return our parsed theme for extension or use else where.
 return theme
